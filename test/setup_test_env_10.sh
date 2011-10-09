dbd6be62ee939e3ad14842fe055a699f223eec4d
Leandro Lameiro <lameiro@gmail.com>
2011-10-09 02:57:00 +0200

    Adds ORACLE_HOME and NLS_LANG to setup env scripts

--- test/setup_test_env_10.sh
+++ test/setup_test_env_10.sh
@@ -1,6 +1,7 @@
 export LD_LIBRARY_PATH=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/lib/
 export PYTHONPATH=/home/lameiro/projects/cx_oracle_on_ctypes/
 export ORACLE_HOME=/usr/lib/oracle/xe/app/oracle/product/10.2.0/server/
+export NLS_LANG=.UTF8
 export CX_ORACLE_USERNAME=cx_Oracle
 export CX_ORACLE_PASSWORD=dev
 export CX_ORACLE_TNSENTRY=XE
--- test/setup_test_env_11.sh
+++ test/setup_test_env_11.sh
@@ -1,6 +1,6 @@
 export LD_LIBRARY_PATH=/home/lameiro/projects/cx_oracle_on_ctypes/instantclient_11_2/
 export PYTHONPATH=/home/lameiro/projects/cx_oracle_on_ctypes/
-
+export NLS_LANG=.UTF8
 export CX_ORACLE_USERNAME=cx_Oracle
 export CX_ORACLE_PASSWORD=dev
 export CX_ORACLE_TNSENTRY=localhost
