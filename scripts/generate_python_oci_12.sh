d62954c7aa3573fd6a779e512d4042b32b03a399
Leandro Lameiro <lameiro@gmail.com>
2016-11-03 02:55:28 +0100

    Improves the ctypesgen script, adds a new oci_generated_12 with Python3 fixes. Fixes issue #14.

--- scripts/generate_python_oci_12.sh
+++ scripts/generate_python_oci_12.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-INSTANTCLIENT_HOME=/home/lameiro/projects/instantclient/instantclient_12_1
+INSTANTCLIENT_HOME=/Users/lameiro/projects/cx_oracle_on_ctypes/instantclient_12_1
+CTYPESGEN_HOME=/Users/lameiro/projects/cx_oracle_on_ctypes/ctypesgen
+SO_NAME=libclntsh.dylib.12.1
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$INSTANTCLIENT_HOME
+export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$INSTANTCLIENT_HOME
 
-python /home/lameiro/projects/ctypesgen/ctypesgen.py $INSTANTCLIENT_HOME/sdk/include/oci.h $INSTANTCLIENT_HOME/sdk/include/ociap.h $INSTANTCLIENT_HOME/sdk/include/ocidfn.h $INSTANTCLIENT_HOME/sdk/include/ori.h $INSTANTCLIENT_HOME/sdk/include/ort.h $INSTANTCLIENT_HOME/sdk/include/orl.h $INSTANTCLIENT_HOME/sdk/include/oro.h -I $INSTANTCLIENT_HOME/sdk/include -o oci_generated_12.py -l libclntsh.so.12.1
+python $CTYPESGEN_HOME/ctypesgen.py $INSTANTCLIENT_HOME/sdk/include/oci.h $INSTANTCLIENT_HOME/sdk/include/ociap.h $INSTANTCLIENT_HOME/sdk/include/ocidfn.h $INSTANTCLIENT_HOME/sdk/include/ori.h $INSTANTCLIENT_HOME/sdk/include/ort.h $INSTANTCLIENT_HOME/sdk/include/orl.h $INSTANTCLIENT_HOME/sdk/include/oro.h -I $INSTANTCLIENT_HOME/sdk/include -o oci_generated_12.py -l $SO_NAME
